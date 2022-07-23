8678d90604e7d96a9a8bcf4c2d05d7ff1c9b9d7f
Farrel Mahaztra <15523645+farrelmahaztra@users.noreply.github.com>
2022-07-23 15:39:43 +0700

    feat: add basic styling with tailwind

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,9 @@ function Header() {
     <header className="py-10">
       <div className="mx-12">
         <nav className="relative z-50 flex justify-between">
-          <h1 className="text-3xl font-title">Omni Tags</h1>
+          <h1 className="text-3xl text-slate-900 font-title">
+            Omni<span className="text-blue-600">Tags</span>
+          </h1>
         </nav>
       </div>
     </header>
