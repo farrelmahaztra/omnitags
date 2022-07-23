2bbc1c08c89a92312edc823d67e6531325c6a353
Farrel Mahaztra <15523645+farrelmahaztra@users.noreply.github.com>
2022-07-23 15:26:06 +0700

    feat: add custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        title: ["Inter", ...defaultTheme.fontFamily.sans],
+        subtitle: ['"Roboto Medium"', ...defaultTheme.fontFamily.sans],
+        body: ['"Roboto Regular"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
