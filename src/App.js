d7161ecdaf85ec6d3c6f38705ac60217eac26041
Farrel Mahaztra <15523645+farrelmahaztra@users.noreply.github.com>
2022-07-23 15:26:13 +0700

    feat: add header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header className="py-10">
+      <div className="mx-12">
+        <nav className="relative z-50 flex justify-between">
+          <h1 className="text-3xl font-title">Omni Tags</h1>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
