66d08f915f42449d4c0909407afaa6623ed74941
Farrel Mahaztra <15523645+farrelmahaztra@users.noreply.github.com>
2022-07-23 15:53:55 +0700

    feat: add nav items

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,16 @@
 function Header() {
   return (
     <header className="py-10">
-      <div className="mx-12">
-        <nav className="relative z-50 flex justify-between">
-          <h1 className="text-3xl text-slate-900 font-title">
+        <nav className="relative z-50 flex mx-12 justify-start items-center">
+          <h1 className="text-3xl text-slate-900 font-title mr-12">
             Omni<span className="text-blue-600">Tags</span>
           </h1>
+          <div className="font-body md:flex md:gap-x-6 items-center">
+            <a className="rounded-lg px-2 py-2 hover:bg-blue-100 hover:text-blue-800" href="#">About</a>
+            <a className="rounded-lg px-2 py-2 hover:bg-blue-100 hover:text-blue-800" href="#">Contact</a>
+            <a className="rounded-lg px-2 py-2 hover:bg-blue-100 hover:text-blue-800" href="#">Donate</a>
+          </div>
         </nav>
-      </div>
     </header>
   );
 }
