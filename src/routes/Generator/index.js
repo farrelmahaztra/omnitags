4e52a7637bc1732864e6e8b1607b3972abe531dd
Farrel Mahaztra <15523645+farrelmahaztra@users.noreply.github.com>
2022-07-24 16:12:43 +0700

    feat: add progress bar footer

--- src/routes/Generator/index.js
+++ src/routes/Generator/index.js
@@ -1,9 +1,34 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import Header from "../../components/Header";
+import Progress from "../../components/Progress";
 
 function Generator() {
+  const [progress, setProgress] = useState(0);
   return (
     <div className="min-h-screen">
       <Header />
+      <div className="absolute inset-x-0 bottom-0 h-16">
+        <div className="flex justify-center">
+          <Link
+            className="rounded-lg text-lg px-2 py-2 bg-blue-200 text-blue-900 hover:bg-blue-100 font-body"
+            to="/generator"
+            onClick={() => setProgress(progress - 25)}
+          >
+            Back
+          </Link>
+          <Progress
+            percentage={progress}
+            />
+          <Link
+            className="rounded-lg text-lg px-2 py-2 bg-blue-200 text-blue-900 hover:bg-blue-100 font-body"
+            to="/generator"
+            onClick={() => setProgress(progress + 25)}
+          >
+            Next
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
