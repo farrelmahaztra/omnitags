1d45ad202631810653391a40691b8f43be8d997f
Farrel Mahaztra <15523645+farrelmahaztra@users.noreply.github.com>
2022-07-24 19:53:48 +0700

    refactor: separate out sidebar into component

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+function Sidebar() {
+  return (
+    <div className="flex flex-auto flex-col min-w-1/12 p-5 border border-r-0 border-slate-200">
+      <Link
+        className="mb-3 p-2 rounded-lg text-slate-600 hover:bg-slate-100"
+        to="basic"
+      >
+        Basic
+      </Link>
+      <Link
+        className="mb-3 p-2 rounded-lg text-slate-600 hover:bg-slate-100"
+        to="advanced"
+      >
+        Advanced
+      </Link>
+      <Link
+        className="mb-8 p-2 rounded-lg text-slate-600 hover:bg-slate-100"
+        to="custom"
+      >
+        Custom
+      </Link>
+      <button className="p-2 rounded-lg text-white bg-blue-500 hover:bg-blue-400">
+        Generate
+      </button>
+    </div>
+  );
+}
+
+export default Sidebar;
--- src/routes/Generator/index.js
+++ src/routes/Generator/index.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import Header from "../../components/Header";
+import Sidebar from "../../components/Sidebar";
 import Progress from "../../components/Progress";
 
 function Generator() {
@@ -9,21 +10,10 @@ function Generator() {
     <div className="min-h-screen flex flex-col">
       <Header />
       <div className="flex flex-auto pb-16">
-        <div className="flex flex-auto flex-col min-w-1/12 p-5 border border-r-0 border-slate-200">
-          <Link className="mb-3 p-2 rounded-lg text-slate-600 hover:bg-slate-100" to="basic">Basic</Link>
-          <Link className="mb-3 p-2 rounded-lg text-slate-600 hover:bg-slate-100" to="advanced">Advanced</Link>
-          <Link className="mb-8 p-2 rounded-lg text-slate-600 hover:bg-slate-100" to="custom">Custom</Link>
-          <button className="p-2 rounded-lg text-white bg-blue-500 hover:bg-blue-400">
-            Generate
-          </button>
-        </div>
+        <Sidebar />
         <div className="flex flex-auto w-11/12 border border-slate-200">
-          <div className="flex-auto bg-blue-500">
-
-          </div>
-          <div className="flex-auto bg-red-500">
-            
-            </div>
+          <div className="flex-auto bg-blue-500"></div>
+          <div className="flex-auto bg-red-500"></div>
         </div>
       </div>
       <div className="absolute inset-x-0 bottom-0 h-16 flex justify-center items-center">
