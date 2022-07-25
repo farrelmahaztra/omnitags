98b8c080d7c2782a80b9ca1019a05c443ffb279b
Farrel Mahaztra <15523645+farrelmahaztra@users.noreply.github.com>
2022-07-25 13:38:24 +0700

    refactor: separate out google preview into component

---
+++ src/components/Preview/Google/index.js
@@ -0,0 +1,35 @@
+function Google({ title, description }) {
+  return (
+    <label className="block mb-5">
+      <span className="text-slate-800">Google</span>
+      <div className="max-w-[600px] flex flex-col relative overflow-hidden mt-5">
+        <div className="leading-[18px] flex">
+          <span className="text-[#202124] font-sans text-sm pr-[5px] cursor-pointer">
+            https://omnitags.io
+          </span>
+          <svg
+            className="fill-[#70757a] h-[22px] w-[22px] cursor-pointer"
+            focusable="false"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"></path>
+          </svg>
+        </div>
+        <div className="flex-1 block overflow-hidden mb-[3px] pt-[5px]">
+          <span className="text-[#1a0dab] font-sans text-xl cursor-pointer">
+            {title || "OmniTags"}
+          </span>
+        </div>
+        <div className="flex-1 block overflow-hidden">
+          <span className="text-[#4d5156] font-sans text-sm">
+            {description ||
+              `Meta tags can be a hassle — it's hard to keep track of which ones help or hurt your SEO ranking. Our tool makes sure your business's website serves up the right tags and unfurls perfectly on every platform.`}
+          </span>
+        </div>
+      </div>
+    </label>
+  );
+}
+
+export default Google;
--- src/routes/Generator/Basic/index.js
+++ src/routes/Generator/Basic/index.js
@@ -1,6 +1,7 @@
 import Header from "../../../components/Header";
 import Sidebar from "../../../components/Sidebar";
 import Footer from "../../../components/Footer";
+import { Google } from "../../../components/Preview";
 
 function Basic({ state, dispatch }) {
   const { title, description } = state;
@@ -55,35 +56,7 @@ function Basic({ state, dispatch }) {
             </label>
           </div>
           <div className="flex flex-col w-2/5 p-6">
-            <label className="block mb-5">
-              <span className="text-slate-800">Google</span>
-              <div className="max-w-[600px] flex flex-col relative overflow-hidden mt-5">
-                <div className="leading-[18px] flex">
-                  <span className="text-[#202124] font-sans text-sm pr-[5px] cursor-pointer">
-                    https://omnitags.io
-                  </span>
-                  <svg
-                    className="fill-[#70757a] h-[22px] w-[22px] cursor-pointer"
-                    focusable="false"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"></path>
-                  </svg>
-                </div>
-                <div className="flex-1 block overflow-hidden mb-[3px] pt-[5px]">
-                  <span className="text-[#1a0dab] font-sans text-xl cursor-pointer">
-                    {title || "OmniTags"}
-                  </span>
-                </div>
-                <div className="flex-1 block overflow-hidden">
-                  <span className="text-[#4d5156] font-sans text-sm">
-                    {description ||
-                      `Meta tags can be a hassle — it's hard to keep track of which ones help or hurt your SEO ranking. Our tool makes sure your business's website serves up the right tags and unfurls perfectly on every platform.`}
-                  </span>
-                </div>
-              </div>
-            </label>
+            <Google title={title} description={description} />
           </div>
         </div>
       </div>
