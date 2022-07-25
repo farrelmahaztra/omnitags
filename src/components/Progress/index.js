0d172b350bc77b138c67b9b5017865341a63663e
Farrel Mahaztra <15523645+farrelmahaztra@users.noreply.github.com>
2022-07-25 16:05:38 +0700

    fix: make progress bar slightly smaller

--- src/components/Progress/index.js
+++ src/components/Progress/index.js
@@ -1,6 +1,6 @@
 function Progress({ percentage = 0 }) {
   return (
-    <div className="w-1/3 h-10 mx-8 rounded-lg border-2 border-slate-200">
+    <div className="w-1/3 h-8 mx-8 rounded-lg border border-slate-200">
       <div
         className="h-full bg-blue-500 rounded-lg"
         style={{ width: `${percentage}%`, transition: "width 250ms ease" }}
