9577fd39bbca3f07e340bed9b3364e76ebb17934
Farrel Mahaztra <15523645+farrelmahaztra@users.noreply.github.com>
2022-07-25 13:45:16 +0700

    fix: improve google text overflow

--- src/components/Preview/Google/index.js
+++ src/components/Preview/Google/index.js
@@ -16,16 +16,19 @@ function Google({ title, description }) {
             <path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"></path>
           </svg>
         </div>
-        <div className="flex-1 block overflow-hidden mb-[3px] pt-[5px]">
-          <span className="text-[#1a0dab] font-sans text-xl cursor-pointer">
+        <div className="flex-1 block  mb-[3px] pt-[5px]">
+          <h3 className="text-[#1a0dab] text-ellipsis overflow-hidden font-sans text-xl cursor-pointer">
             {title || "OmniTags"}
-          </span>
+          </h3>
         </div>
-        <div className="flex-1 block overflow-hidden">
-          <span className="text-[#4d5156] font-sans text-sm">
+        <div className="flex-1 block">
+          <p
+            className="text-[#4d5156] font-sans text-sm"
+            style={{ overflowWrap: "break-word" }}
+          >
             {description ||
               `Meta tags can be a hassle — it's hard to keep track of which ones help or hurt your SEO ranking. Our tool makes sure your business's website serves up the right tags and unfurls perfectly on every platform.`}
-          </span>
+          </p>
         </div>
       </div>
     </label>
