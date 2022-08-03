31ca510dbb7481b1eb5ac717af67f5c4406ccb7c
Farrel Mahaztra <15523645+farrelmahaztra@users.noreply.github.com>
2022-08-03 16:53:15 +0700

    feat: add selectinput

---
+++ src/components/SelectInput/index.js
@@ -0,0 +1,15 @@
+function SelectInput({ onChange, value, options }) {
+  return (
+    <select
+      class="mt-1 block w-full rounded-md bg-slate-100 border-transparent focus:border-slate-200 focus:ring-0"
+      onChange={onChange}
+      value={value}
+    >
+      {options.map((option) => (
+        <option value={option.value}>{option.label}</option>
+      ))}
+    </select>
+  );
+}
+
+export default SelectInput;
