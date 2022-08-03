5f921db07c4e80387d286065f34fb517ba547a8e
Farrel Mahaztra <15523645+farrelmahaztra@users.noreply.github.com>
2022-08-03 18:24:39 +0700

    fix: fix slack preview border line

--- src/components/Preview/Slack/index.js
+++ src/components/Preview/Slack/index.js
@@ -6,7 +6,7 @@ function Slack({ title, description, imageUrl, siteName }) {
     <label className="block mb-5">
       <span className="text-slate-800">Slack</span>
       <div className="max-w-[600px] min-h-[304px] flex relative overflow-hidden mt-5">
-        <div className="w-[12px] min-h-[304px] rounded-[8px] bg-[#dddddd]"></div>
+        <div className="w-[4px] min-h-[304px] rounded-[8px] bg-[#dddddd]"></div>
         <div className="flex flex-col px-[12px] leading-[1.46667]">
           <p className="font-bold">{siteName || title}</p>
           <Link className="text-[#1264a3] font-bold" to="#">
