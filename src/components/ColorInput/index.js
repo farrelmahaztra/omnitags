3996901623ea84ba26134ecf504eb1ac3ff3ab9c
Farrel Mahaztra <15523645+farrelmahaztra@users.noreply.github.com>
2022-09-05 22:58:14 +0700

    feat: add colorpicker

---
+++ src/components/ColorInput/index.js
@@ -0,0 +1,12 @@
+function ColorInput({ onChange, value }) {
+  return (
+    <input
+      class="mt-1 block w-full rounded-md bg-slate-100 border-transparent focus:border-slate-200 focus:ring-0 cursor-pointer"
+      type="color"
+      onChange={onChange}
+      value={value}
+    />
+  );
+}
+
+export default ColorInput;
