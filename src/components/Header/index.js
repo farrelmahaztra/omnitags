873011e8dd11667727a3a24f719379b8e254c387
Farrel Mahaztra <15523645+farrelmahaztra@users.noreply.github.com>
2022-10-09 17:09:07 +0700

    fix: fix mobile header link spacing

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 const Header = () => (
   <header className="flex h-[8vh]">
-    <nav className="relative z-50 flex mx-12 justify-start items-center">
+    <nav className="relative z-50 flex mx-6 md:mx-12 w-full justify-between md:justify-start items-center">
       <Link to="/">
         <h1 className="text-3xl text-slate-900 font-title mr-12">
           Omni<span className="text-[#3b82f6]">Tags</span>
         </h1>
       </Link>
-      <div className="font-body md:flex md:gap-x-6 text-lg items-center">
+      <div className="font-body md:flex gap-x-6 text-lg items-center">
         <Link
           className="p-2 rounded-lg text-slate-600 hover:bg-slate-100"
           to="/about"
