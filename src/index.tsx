b04c7507b9d130560b8693aa6cfe439d17383f4f
Farrel Mahaztra <15523645+farrelmahaztra@users.noreply.github.com>
2022-10-11 20:06:25 +0700

    chore: remove web-vitals

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./routes/About";
 import Generator from "./routes/Generator";
-import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 
 const rootElement = document.getElementById("root") as Element;
@@ -16,8 +15,3 @@ root.render(
     </Routes>
   </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
